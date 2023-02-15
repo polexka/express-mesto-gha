8f7d8ff7b192fa449cd5399c98758517c3aa50cf
polechka <pol1na-tsukanova@yandex.ru>
2023-02-16 00:10:18 +0300

    undo changes

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // eslint-disable-next-line import/no-extraneous-dependencies
-const validator = require('validator');
+// const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -19,21 +19,21 @@ const userSchema = new mongoose.Schema({
     minlength: 2,
     maxlength: 30,
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: {
-      validator: (email) => validator.isEmail(email),
-      message: 'Введен некорректный email',
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
-    select: false,
-  },
+  // email: {
+  //   type: String,
+  //   required: true,
+  //   unique: true,
+  //   validate: {
+  //     validator: (email) => validator.isEmail(email),
+  //     message: 'Введен некорректный email',
+  //   },
+  // },
+  // password: {
+  //   type: String,
+  //   required: true,
+  //   minlength: 8,
+  //   select: false,
+  // },
 });
 
 module.exports = mongoose.model('user', userSchema);
