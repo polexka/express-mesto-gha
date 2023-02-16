92073576196c53224c212f987b6348faeb7314d6
polechka <pol1na-tsukanova@yandex.ru>
2023-02-16 10:00:38 +0300

    fixed email validation

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.post('/signin', celebrate({
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required(),
+    email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
