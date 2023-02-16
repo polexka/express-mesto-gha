443e13cd9c8b9906c60ae99ad54538413a4a8702
polechka <pol1na-tsukanova@yandex.ru>
2023-02-16 07:47:42 +0300

    eslint fix

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
     authorization: `Bearer ${req.cookies.token}`,
   };
   req.user = {
-    _id: '5d8b8592978f8bd833ca8133'
+    _id: '5d8b8592978f8bd833ca8133',
   };
   next();
 });
