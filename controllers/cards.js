d20b5e5681f396ae05fde22a7290c4a0bd7d5062
polechka <pol1na-tsukanova@yandex.ru>
2023-02-18 17:32:55 +0300

    create card when create card instead user_id -> user

--- controllers/cards.js
+++ controllers/cards.js
@@ -13,7 +13,7 @@ module.exports.getCards = (req, res, next) => {
 module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
 
-  Card.create({ name, link, owner: req.user._id })
+  Card.create({ name, link, owner: req.user })
     .then((card) => res.send(card))
     .catch(next);
 };
