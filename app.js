880782252e82c8a3eff2be6477eb322ba0558b9f
polechka <pol1na-tsukanova@yandex.ru>
2023-02-19 15:51:22 +0300

    auth user

--- app.js
+++ app.js
@@ -61,11 +61,11 @@ app.post('/signup', celebrate({
 app.use(auth);
 
 app.use((req, res, next) => {
-  const userObj = getUserInfo(req, res);
+  // const userObj = getUserInfo(req, res);
+  return res.status(500).send({message: req.user});
+  // req.user = userObj;
 
-  req.user = userObj;
-
-  return next();
+  // return next();
 });
 
 app.use('/users', require('./routes/users'));
